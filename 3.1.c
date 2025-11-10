a9dbc197bee4bb81a418d3b7defbfc9aa6dc4560
Эксузян Тимур Артемович ТКИ-141 <timka.eksuzyan.00@mail.ru>
2025-11-10 14:02:39 +0300

    Add files via upload

---
+++ 3.1.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <float.h>
+#include <stdbool.h>
+#include <math.h>
+/**
+*@brief Считывает значение, введенное с клавиатуры с проверкой ввода
+*@return считанное значение
+*/
+double value();
+/**
+*@brief Рассчитывает значение по заданной функции
+*@param x значение переменной
+*@return рассчитанное значение переменной
+*/
+double func(const double x);
+/**
+ * @brief проверяет,что переменная положительная
+ * @param step значение проверяемой переменной
+ */
+void steps(const double step);
+/**
+ * @brief Точка входа в программу
+ * @param start начальное значение функции
+ * @param end конечное значение функции
+ * @param step значение шага функции
+ * @return 0, если программа выполнена корректно, иначе 1
+ */
+int main()
+{
+    printf("Введите начальное значение:");
+    double start = value();
+    printf("Введите конечное значение:");
+    double end = value();
+    printf("Введите шаг:");
+    double step = value();
+    steps(step);
+    double x = start;
+    while (x < end + DBL_EPSILON)
+	{
+		printf("x = %.2lf, y = %.4lf\n", x, func(x));
+        x = x + step;
+	}
+    return 0;
+}
+double value()
+{
+    double value = 0;
+    if (!scanf("%lf",&value))
+    {
+        printf("Вы ввели неверное значение!\n");
+        abort();
+    }
+    return value;
+}
+void steps(const double step)
+{
+    if (step <= DBL_EPSILON)
+	{
+		printf("Ошибка! Шаг должен быть положительным!\n");
+		abort();
+	}
+}
+double func(const double x)
+{
+    return 0.29 * pow(x,3) + x - 1.2502;
+}
